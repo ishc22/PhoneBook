e4b56238e53f0f60653c0f0024b010f71d885de4
Isham_Cespedes <ishamscespedes@gmail.com>
2023-03-23 18:35:13 -0500

    finally working

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db 
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
@@ -7,6 +8,13 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     address = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.Integer(50), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    def __repr__(self):
+        return f'User {self.id} {self.first_name} {self.last_name}'
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
     
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, url_for, flash
 from app.forms import PhoneNumberForm
+from app.models import User
 
 @app.route("/")
 def index():
@@ -13,7 +14,12 @@ def phone_number():
         first_name = form.first_name.data 
         last_name = form.last_name.data 
         address = form.address.data 
-        phone_number  = form.phone_number .data 
-        flash("Success!!!Phone number has been added to the Phone Book.")
+        phone_number  = form.phone_number.data 
+        check_number = db.session.execute(db.select(User).filter(User.phone_number == phone_number)).scalars().all()
+        if check_number:
+            flash('Phone Number already exists🥲!')
+            return redirect(url_for('phone_number'))
+        new_user = User(first_name=first_name, last_name=last_name, address=address, phone_number=phone_number)
+        flash(f"Success😁 Phone number {phone_number} has been added to the Phone Book!")
         return redirect(url_for('index'))
     return render_template('phone_number.html', form=form)
